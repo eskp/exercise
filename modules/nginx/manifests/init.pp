c7be1c5c393b942874a2414d83012e1a5d0c9a04
eskp <si@eskp.net>
2014-06-03 17:22:53 +1000

    Update init.pp

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,4 +1,34 @@
-# Main class to setup nginx Web Server
+# == Class: nginx
+#
+# Base class for Nginx.
+# Installs the package(s), configures files/directories,
+# enables and starts the service.
+#
+# === Parameters
+#
+# [*conf_dir*]
+#   (string) Nginx configuration directory
+#   Defaults to '/etc/nginx' ($::nginx::conf_dir)
+#
+# [*user*]
+#   (string) Nginx system user
+#   Defaults to 'www-data' ($::nginx::user)
+#
+# [*access_log*]
+#   (string) Nginx access log file path
+#   Defaults to '/var/log/nginx/access.log' ($::nginx::access_log)
+#
+# [*error_log*]
+#   (string) Nginx error log file path
+#   Defaults to '/var/log/nginx/error.log' ($::nginx::error_log)
+#
+# === Example
+#
+# class { 'nginx':
+#   conf_dir => '/etc/httpd',
+#   user     => 'www',
+# }
+#
 
 class nginx(
   $conf_dir   = '/etc/nginx',
