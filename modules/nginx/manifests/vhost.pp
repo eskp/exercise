38f8abe2c980ebf6d5b2ca901156158783e643aa
eskp <si@eskp.net>
2014-06-03 17:48:41 +1000

    Update vhost.pp

--- modules/nginx/manifests/vhost.pp
+++ modules/nginx/manifests/vhost.pp
@@ -1,10 +1,29 @@
-# Definition: nginx::vhost
+# == Definition: nginx::vhost
 #
-# This class install Nginx Virtual Host configuration
+# Installs Nginx Virtual Host template in
+# Nginx site directory ($::nginx::conf_dir)/conf.d
+#
+# === Parameters
+#
+# [*docroot*]
+#   (string) Document root directory where to serve files from
+#   Defaults to '/var/www' ($::nginx::vhost::docroot)
+#
+# [*port*]
+#   (int) Nginx virtual host port
+#   Defaults to 8000 ($::nginx::vhost::port)
+#
+# === Example
+#
+# nginx::vhost { 'my_site':
+#   docroot => '/srv/www',
+#   port    => 8080,
+# }
+#
 
 define nginx::vhost(
   $docroot='/var/www',
-  $port='8000',
+  $port=8000,
 ) {
 
   include nginx
